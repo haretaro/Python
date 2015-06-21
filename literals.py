a8ac2deabd075ff6d65072715daf01f6d9ddef69
haretaro <frontend.1000@gmail.com>
2015-06-22 00:42:26 +0900

    add literals.py

---
+++ literals.py
@@ -0,0 +1,33 @@
+import fractions
+print('----Bool------')
+print(True)
+print(False)
+print('----Integer-----')
+print(1)
+print('----Floating Point-----')
+print(3.14)
+print('----Fraction----')
+print(fractions.Fraction(1,3))
+print('----List----')
+print( ['a','b',1,3.14] )
+print('----Tuple----')
+print( ('a','b',1,3.14) )
+print('----Set----')
+print( {'a','b',1,3.14} )
+print('----Dictionary----')
+print( {'pi':3.14, 'e':2.71} )
+print('----None----')
+print(None)
+print(None == True)
+print(None == False)
+print(None == None)
+print('----String----')
+print('shortstring')
+print("shortstring")
+print(
+'''long
+string''')
+print(
+"""long
+string""")
+
