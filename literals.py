6026fc53132f6c6fbab80ba08349c8768e244f04
haretaro <frontend.1000@gmail.com>
2015-06-22 00:48:15 +0900

    added comments

--- literals.py
+++ literals.py
@@ -1,26 +1,36 @@
 import fractions
+
 print('----Bool------')
 print(True)
 print(False)
+
 print('----Integer-----')
 print(1)
+
 print('----Floating Point-----')
 print(3.14)
+
 print('----Fraction----')
 print(fractions.Fraction(1,3))
+
 print('----List----')
 print( ['a','b',1,3.14] )
-print('----Tuple----')
+
+print('----Tuple----') #tuple is imutable list
 print( ('a','b',1,3.14) )
-print('----Set----')
+
+print('----Set----') #set is unorderd values
 print( {'a','b',1,3.14} )
-print('----Dictionary----')
+
+print('----Dictionary----') #a kind of hash
 print( {'pi':3.14, 'e':2.71} )
+
 print('----None----')
 print(None)
-print(None == True)
-print(None == False)
-print(None == None)
+print(None == True) #False
+print(None == False) #False
+print(None == None) #True
+
 print('----String----')
 print('shortstring')
 print("shortstring")
