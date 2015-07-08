eec0c306a79cc93f3978cfee28aea141354fb82b
haretaro <frontend.1000@gmail.com>
2015-07-08 16:07:45 +0900

    iterator

--- mylist.py
+++ mylist.py
@@ -3,13 +3,27 @@ class Node:
 
     def __init__(self):
         self.data = None
-        self.next = None
+        self.nextnode = None
+        self.i = 1
 
     def __str__(self):
         return str(self.data)
 
     def hasNext(self):
-        return self.next!=None
+        return self.nextnode!=None
+
+class ListIterator:
+    """Iterator of List class"""
+    
+    def __init__(self,node):
+        self.node = node
+
+    def next(self):
+        if self.hasNext():
+            raise StopIteration()
+        temp = self.node.data
+        self.node = self.node.nextnode
+        return temp
 
 class List:
     """Sequential List defined by me"""
@@ -28,16 +42,19 @@ class List:
         node = self.__head
         while node.hasNext():
             s += str(node)+', '
-            node = node.next
+            node = node.nextnode
         return s[:-2]+']'
 
     def __repr__(self):
         return self.__str__()
 
+    def __iter__(self):
+        return ListIterator(self.__head)
+
     def add(self,data):
         self.__end.data = data
-        self.__end.next = Node()
-        self.__end = self.__end.next
+        self.__end.nextnode = Node()
+        self.__end = self.__end.nextnode
         self.__length += 1
         return self
 
@@ -50,7 +67,7 @@ class List:
         node = self.__head
         for i in range(n):
             if node.hasNext():
-                node = node.next
+                node = node.nextnode
             else:
                 raise IndexError
         return node.data
@@ -64,3 +81,6 @@ if __name__ == '__main__':
     print(l)
     print('list[0] = '+str(l.get(0)))
     print('length = '+str(l.getLength()))
+    print('iteration')
+    for x in l:
+        print(x)
