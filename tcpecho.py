161c365e6390e28801f2355a349b8b967e79d9f5
haretaro <frontend.1000@gmail.com>
2015-11-28 23:02:46 +0900

    added tcpecho.py

---
+++ tcpecho.py
@@ -0,0 +1,20 @@
+import socket
+
+host = '127.0.0.1'
+port = 8080
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind((host,port))
+serversocket.listen(5)
+
+clientsocket, address = serversocket.accept()
+
+print('connected by ', address)
+
+while True:
+    data = clientsocket.recv(1024)
+    if not data: break
+    print(data)
+    clientsocket.send(data)
+
+clientsocket.close()
+
