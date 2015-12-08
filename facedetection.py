48f4d3f4cca1214f58d8f1dea5f46d56d0263091
haretaro <frontend.1000@gmail.com>
2015-12-08 11:06:31 +0900

    opencv3で使えるように書きなおした

--- facedetection.py
+++ facedetection.py
@@ -5,8 +5,9 @@ cascade_path = '/usr/local/Cellar/opencv/2.4.12/share/OpenCV/haarcascades/haarca
 cascade = cv2.CascadeClassifier(cascade_path)
 
 def detect(img,color=(255,0,0)):
-    img_gray = cv2.cvtColor(img, cv2.cv.CV_BGR2GRAY)
-    facerect = cascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=1, minSize=(20,20))
+    #img_gray = cv2.cvtColor(img, cv2.cv.CV_BGR2GRAY)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    facerect = cascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=1, minSize=(60,60))
     if len(facerect) > 0:
         for rect in facerect:
             cv2.rectangle(img, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
