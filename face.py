5d93abb96a4175876c74b7a35a412ae7b7e967c2
haretaro <frontend.1000@gmail.com>
2016-05-17 18:08:59 +0900

    cascadeのディレクトリが変わってたので修正

--- face.py
+++ face.py
@@ -5,7 +5,7 @@ import sys
 
 img = cv2.imread(sys.argv[1])
 
-cascade_path = '/usr/local/Cellar/opencv/2.4.12/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'
+cascade_path = '/usr/local/Cellar/opencv/2.4.12_2/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'
 cascade = cv2.CascadeClassifier(cascade_path)
 color = (255,128,0)
 img_gray = cv2.cvtColor(img, cv2.cv.CV_BGR2GRAY)
--- facedetection.py
+++ facedetection.py
@@ -1,6 +1,6 @@
 import cv2
 
-cascade_path = '/usr/local/Cellar/opencv/2.4.12/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'
+cascade_path = '/usr/local/Cellar/opencv/2.4.12_2/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'
 
 cascade = cv2.CascadeClassifier(cascade_path)
 
@@ -12,3 +12,23 @@ def detect(img,color=(255,0,0)):
         for rect in facerect:
             cv2.rectangle(img, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
     return img
+
+def get_face_image(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    facerect = cascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=1, minSize=(60,60))
+
+    max = 0
+    maxh = 0
+    maxw = 0
+    resx = 0
+    resy = 0
+    if len(facerect) < 1:
+        return None
+    for (x,y,w,h) in facerect:
+        if max<w*h:
+            maxw = w
+            maxh = h
+            resx = x
+            resy = y
+            max = w*h
+    return img[resy:resy+maxh, resx:resx+maxw]
