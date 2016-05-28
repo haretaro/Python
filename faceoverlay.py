e006142fcc7669f9e9244589305f19aa57e69f3a
haretaro <frontend.1000@gmail.com>
2016-05-28 15:29:51 +0900

    顔合成するスクリプト

---
+++ faceoverlay.py
@@ -0,0 +1,36 @@
+#coding:utf-8
+import cv2
+from overlayImage import overlay
+cap = cv2.VideoCapture(0)
+cap.set(3, 800) #width
+cap.set(4, 450) #height
+scale = 1.7 #顔の拡大率
+faceoffset = 0.05 #顔の上下移動(顔何個分)
+
+cascade_path = '/usr/local/Cellar/opencv/2.4.12_2/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml'
+cascade = cv2.CascadeClassifier(cascade_path)
+face = cv2.imread('cage.png',-1)
+
+key = 0
+while True:
+    if key == ord('q'):
+        break
+    grabbed, frame = cap.read()
+    if not grabbed:
+        break
+    img_gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    facerect = cascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=1, minSize=(60,60))
+    if len(facerect) > 0:
+        for rect in facerect:
+            size = tuple(int(x*scale) for x in rect[2:4])
+            offset = rect[0:2] - rect[2:4] * ((scale-1)/2)
+            offset[1] += size[1] * faceoffset
+            offset = tuple(int(x) for x in offset)
+            frame = overlay(frame, face, offset, size)
+    cv2.imshow('frame',frame)
+    key = cv2.waitKey(100) & 0xFF
+
+cap.release()
+
+
+
--- overlayImage.py
+++ overlayImage.py
@@ -1,8 +1,20 @@
 import cv2
 import numpy as np
 
-def overlay(src, dst, offset, size):
+def overlay(dst, src, offset, size):
+    offset = offset[1],offset[0]
     src = cv2.resize(src, size)
+    offset = list(offset)
+    if(offset[0] < 0):
+        src = src[-offset[0]:]
+        offset[0] = 0
+    if(offset[1] < 0):
+        src = src[:,-offset[1]:]
+        offset[1] = 0
+    if(offset[0] + size[1] > dst.shape[0]):
+        src = src[: src.shape[0] - (offset[0] + size[1] - dst.shape[0])]
+    if(offset[1] + size[0] > dst.shape[1]):
+        src = src[:, : src.shape[1] - (offset[1] + size[0] - dst.shape[1])]
     temp = [src[:,:,c] * (src[:,:,3] / 255) + dst[offset[0] : offset[0] + src.shape[0], offset[1] : offset[1] + src.shape[1] , c] * (1.0 - src[:,:,3]/255) for c in range(3)]
     temp = np.asarray(temp,dtype=np.uint8)
     temp = temp.transpose((1,2,0))
@@ -13,6 +25,7 @@ def overlay(src, dst, offset, size):
 if __name__ == '__main__':
     src = cv2.imread('cage.png',-1)#use alpha channel
     dst = cv2.imread('hoge.jpg')
-    result = overlay(src, dst, (20,20), (300,300))
+    print(dst.shape)
+    result = overlay(dst, src, (40,30), (250,270))
     cv2.imshow('cage',result)
     cv2.waitKey(0)
