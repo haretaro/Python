36aca891d12d6471f1091deb7375c9c6b58580a2
haretaro <frontend.1000@gmail.com>
2016-06-18 18:16:36 +0900

    add multiprocessing_sample.py

---
+++ multiprocessing_sample.py
@@ -0,0 +1,19 @@
+#condig: utf-8
+#プロセス間でパイプで通信するテスト
+from multiprocessing import Process, Pipe
+
+def f(conn):
+    counter = 0
+    while True:
+        conn.send([42, None, 'Hello', counter])
+        counter += 1
+    #conn.close()
+
+parent_conn, child_conn = Pipe()
+p = Process(target=f, args=(child_conn,))
+p.start()
+for _ in range(5):
+    print(parent_conn.recv())
+p.join(2)#ブロックする.引数はタイムアウト
+p.terminate()
+print('hoge')
